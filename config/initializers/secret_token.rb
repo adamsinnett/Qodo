b8806f609183d8a0c9bcd57e24178291fdd14985
Adam Sinnett <quandrum@gmail.com>
2011-09-29 19:33:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qodo::Application.config.secret_token = '747820e327cdd80d940d3f615ede46f8891c34952c31b7fb51d27c3383b8efc36ffeec7c13c89953db21a366bb17d3ed029d8a0a54400c888857f6e23c5f13be'
